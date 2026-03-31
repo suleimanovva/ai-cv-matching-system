08e1f194a0e91079848e120fd16cfd9e3e058a14
suleimanovva <suleimanovanigara@icloud.com>
2026-03-31 13:36:57 +0300

    feat: finalize bias detection module and audit UI

---
+++ CvMatchingSystem/Migrations/20260331095746_AddDemographicsForFairness.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CvMatchingSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDemographicsForFairness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Candidates",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Candidates",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Candidates");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Candidates");
+        }
+    }
+}
